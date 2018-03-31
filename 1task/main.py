80af2a148308c05463702fedde2ce1c3d7fc7740
tmazza <tiagomdepaula@gmail.com>
2018-03-31 15:50:50 -0300

    Join canais

--- 1task/main.py
+++ 1task/main.py
@@ -1,11 +1,15 @@
+import time
 import matplotlib.pyplot as plt
 import numpy as np
 from rawkit.raw import Raw
+
+start_time = time.time()
 
 with Raw(filename='a.CR2') as raw:
 
   img_raw, color_array_scheme = raw.bayer_data()
   img_raw = np.array(img_raw);
+  img_raw = img_raw / 4096;
 
   img_r = np.copy(img_raw);
   img_r[1:-1:2, ::2] = (img_r[:-2:2, ::2] + img_r[2::2, ::2]) / 2.
@@ -21,36 +25,18 @@ with Raw(filename='a.CR2') as raw:
   img_g[2::2, 2::2] = (img_g[2::2, 1:-2:2] + img_g[2::2, 3::2] + img_g[1:-2:2, 2::2] + img_g[3::2, 2::2]) / 4.
   img_g[1:-1:2, 1:-1:2] = (img_g[1:-1:2, :-3:2] + img_g[1:-1:2, 2::2] + img_g[:-2:2, 1:-1:2] + img_g[2::2, 1:-1:2]) / 4.
 
-  # plt.axis('off')
-  # plt.imshow(img_r/16, cmap='Reds')
-  # plt.show()
-  
-  # plt.axis('off')
-  # plt.imshow(img_g/16, cmap='Greens')
-  # plt.show()
+  rw = img_r[200][1200]; print("R", rw);
+  gw = img_g[200][1200]; print("G", gw);
+  bw = img_b[200][1200]; print("B", bw);
 
-  # plt.axis('off')
-  # plt.imshow(img_b/16, cmap='Blues')
-  # plt.show()
+  img_r = ( img_r * (1/rw) ) ** (2.2)
+  img_g = ( img_g * (1/gw) ) ** (2.2)
+  img_b = ( img_b * (1/bw) ) ** (2.2)
 
-  rw = img_r[200][1200]
-  gw = img_g[200][1200]
-  bw = img_b[200][1200]
-  print("R", rw);
-  print("G", gw);
-  print("B", bw);
+  img_rgb = np.stack([img_r, img_g, img_b], axis=2)
 
-  # Composição das cores + white balance + gamma correction
-  # mgb = [[[] for x in range(cols)] for y in range(rows)]
-  # for i in range(0, rows):
-  #   for j in range(0, cols):
-  #     # white balance && gamma correction
-  #     mgb[i][j] = [
-  #       int( ( (m[i][j]*(1/rw) )**(1/2.2)) * 255 ),
-  #       int( ( (img_g[i][j]*(1/gw) )**(1/2.2)) * 255 ),
-  #       int( ( (img_b[i][j]*(1/bw) )**(1/2.2)) * 255 )
-  #     ]
+  print("Decorrido", time.time() - start_time, "seg")
 
-  # plt.axis('off')
-  # plt.imshow(mgb)
-  # plt.show()
+  plt.axis('off')
+  plt.imshow(img_rgb)
+  plt.show()
