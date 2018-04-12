4443b10c83551c55ca4f35998436e605efb9386a
tmazza <tiagomdepaula@gmail.com>
2018-04-12 11:14:38 -0300

    ignore + ajustes

--- 2task/main.py
+++ 2task/main.py
@@ -19,12 +19,7 @@ for line in file:
   curva_b[i] = b
   i+=1
 
-curva_r = 10**curva_r
-print(curva_r)
-
-'''
-
-# Valor pixel linearizado
+# Valor pixel linearizado - gamaa decoding
 im = Image.open("images/office_1.jpg")
 pix = im.load()
 width, height = im.size
@@ -37,40 +32,37 @@ for i in range(0, width):
   for j in range(0, height):
     r[i, j], g[i, j], b[i, j] = pix[i, j]
 
-r = ( (r/255)**2.2 ) * 255
-g = ( (g/255)**2.2 ) * 255
-b = ( (b/255)**2.2 ) * 255
+r = 255 * np.power(r/255, 2.2)
+g = 255 * np.power(g/255, 2.2)
+b = 255 * np.power(b/255, 2.2)
 
 # Cálculo irradiância
 # TODO: melhorar existem somente 256 valores possíveis salvar mapeamentos
-
-def get_closest(exposure_value, curve):
+def index_response_curve(exposure_value, curve):
   for i in range(1, len(curve)):
     if(curve[i] > exposure_value):
       curr = abs(exposure_value - curve[i])
       prev = abs(exposure_value - curve[i-1])
       return i if(curr < prev) else i-1
   return 255
-print(r)
 
 for i in range(0, width):
   for j in range(0, height):
-    r[i, j] = get_closest(r[i, j], curva_r)
-    g[i, j] = get_closest(g[i, j], curva_g)
-    b[i, j] = get_closest(b[i, j], curva_b)
+    r[i, j] = index_response_curve(r[i, j], curva_r)
+    g[i, j] = index_response_curve(g[i, j], curva_g)
+    b[i, j] = index_response_curve(b[i, j], curva_b)
 
-r = r / 0.03
-g = g / 0.03
-b = b / 0.03
+r = np.exp(r) / 0.03
+g = np.exp(g) / 0.03
+b = np.exp(b) / 0.03
 
-print(r)
-print(g)
-print(b)
+print(r.min())
+print(r.max())
 
-img_rgb = np.stack([img_r, img_g, img_b], axis=2)
+# rgb = np.stack([r, g, b], axis=2)
 
-plt.axis('off')
-plt.imshow(img_rgb)
-plt.show()
+# print(rgb)
 
-'''
+# plt.axis('off')
+# plt.imshow(rgb)
+# plt.show()
